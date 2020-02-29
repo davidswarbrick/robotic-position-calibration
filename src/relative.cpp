1b538ca803ae605337d272123ffb3a6c901aa225
David Swarbrick <david.swarbrick9@gmail.com>
2020-02-29 13:04:26 +0000

    Updated copyright statement

--- src/relative.cpp
+++ src/relative.cpp
@@ -1,21 +1,9 @@
 /*******************************************************************************
+*   Based on code under copyrights below, supplied as part of the Chilitags    *
+*   project: https://github.com/chili-epfl/chilitags                           *
 *   Copyright 2013-2014 EPFL                                                   *
 *   Copyright 2013-2014 Quentin Bonnard                                        *
-*                                                                              *
-*   This file is part of chilitags.                                            *
-*                                                                              *
-*   Chilitags is free software: you can redistribute it and/or modify          *
-*   it under the terms of the Lesser GNU General Public License as             *
-*   published by the Free Software Foundation, either version 3 of the         *
-*   License, or (at your option) any later version.                            *
-*                                                                              *
-*   Chilitags is distributed in the hope that it will be useful,               *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of             *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
-*   GNU Lesser General Public License for more details.                        *
-*                                                                              *
-*   You should have received a copy of the GNU Lesser General Public License   *
-*   along with Chilitags.  If not, see <http://www.gnu.org/licenses/>.         *
+*   Additions made by David Swarbrick 2020                                     * 
 *******************************************************************************/
 
 #include <chilitags.hpp>
